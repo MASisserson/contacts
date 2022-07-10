0317f7d7cc12abe66a3b5aa79c220b9cde8f96ea
Matthew Sisserson <masisserson@gmail.com>
2022-07-10 10:27:12 -0400

    Fixed a bug where the new contact form would not accept all acceptable phone input formats and altered the schema to not include manually inserted data.

--- schema.sql
+++ schema.sql
@@ -4,9 +4,6 @@ CREATE TABLE users (
   "password" text
 );
 
-INSERT INTO users (id, username, "password")
-VALUES (1, 'admin', '$2a$12$wZEWwTmygfVvpchJQ56lT.j8VMNloUfrIAK/rWArL/deD7y9sZf4y');
-
 CREATE TABLE contacts (
   id serial PRIMARY KEY,
   "name" text NOT NULL,
@@ -18,20 +15,8 @@ CREATE TABLE contacts (
 
 ALTER TABLE contacts ADD UNIQUE ("user_id", phone);
 
-INSERT INTO contacts ("name", phone, email, category, "user_id")
-VALUES ('Hank Hill', '1111111111', 'king@hill.com', 'Friend', 1),
-       ('Socrates', '1234567890', 'philosopher@forms.com', 'Work', 1),
-       ('Paul', NULL, NULL, NULL, 1),
-       ('Frank', '4444444444', NULL, NULL, 1);
-
 CREATE TABLE contact_ids (
   id serial PRIMARY KEY,
   uuid varchar(20) UNIQUE NOT NULL,
   contact_id integer REFERENCES contacts(id) ON DELETE CASCADE
 );
-
-INSERT INTO contact_ids (uuid, contact_id)
-VALUES ('142d2f6c6b3f18cbe773', 1),
-       ('faba80cb755e43ac0355', 2),
-       ('cd0318e98130189b99a4', 3),
-       ('8c8dd9edc6ad176fa1db', 4);
